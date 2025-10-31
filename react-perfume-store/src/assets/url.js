fb9cc41baf96866ac0fe91d4e72e32cb8a362067
Youssef Eraky <yeraky07@gmail.com>
2025-10-30 22:30:14 -0700

    removed

--- react-perfume-store/src/assets/url.js
+++ react-perfume-store/src/assets/url.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:5000/api"
-export const BASE_URL = "https://perfume-backend-eight.vercel.app/api"
+export const BASE_URL = "http://localhost:5000/api"
+// export const BASE_URL = "https://perfume-backend-eight.vercel.app/api"
